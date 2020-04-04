bff520fd2c0993ab189adecf9cd7173bac995878
jackson <jacksonfoster87@gmail.com>
2020-04-04 00:14:14 -0400

    files

---
+++ Form1.Designer.cs
@@ -0,0 +1,306 @@
+﻿namespace Blackjack
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.DrawButton = new System.Windows.Forms.Button();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.TotalText = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.Card6 = new System.Windows.Forms.Label();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.Card5 = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.Card4 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.Card3 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.Card2 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.Card1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel9.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel8.SuspendLayout();
+            this.panel7.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AccessibleName = "Panel1";
+            this.panel1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.panel1.Controls.Add(this.DrawButton);
+            this.panel1.Controls.Add(this.panel9);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(-1, -59);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1235, 612);
+            this.panel1.TabIndex = 0;
+            // 
+            // DrawButton
+            // 
+            this.DrawButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DrawButton.Location = new System.Drawing.Point(882, 433);
+            this.DrawButton.Name = "DrawButton";
+            this.DrawButton.Size = new System.Drawing.Size(288, 151);
+            this.DrawButton.TabIndex = 4;
+            this.DrawButton.Text = "Begin";
+            this.DrawButton.UseVisualStyleBackColor = true;
+            this.DrawButton.Click += new System.EventHandler(this.DrawButton_Click);
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.FloralWhite;
+            this.panel9.Controls.Add(this.TotalText);
+            this.panel9.Location = new System.Drawing.Point(640, 433);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(155, 151);
+            this.panel9.TabIndex = 3;
+            // 
+            // TotalText
+            // 
+            this.TotalText.AutoSize = true;
+            this.TotalText.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TotalText.Location = new System.Drawing.Point(63, 63);
+            this.TotalText.Name = "TotalText";
+            this.TotalText.Size = new System.Drawing.Size(30, 29);
+            this.TotalText.TabIndex = 4;
+            this.TotalText.Text = "X";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 473);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(590, 69);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Your current amount:";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Wheat;
+            this.panel2.Controls.Add(this.panel8);
+            this.panel2.Controls.Add(this.panel7);
+            this.panel2.Controls.Add(this.panel6);
+            this.panel2.Controls.Add(this.panel5);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(0, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1298, 398);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.FloralWhite;
+            this.panel8.Controls.Add(this.Card6);
+            this.panel8.Location = new System.Drawing.Point(1015, 191);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(155, 151);
+            this.panel8.TabIndex = 2;
+            // 
+            // Card6
+            // 
+            this.Card6.AutoSize = true;
+            this.Card6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Card6.Location = new System.Drawing.Point(67, 64);
+            this.Card6.Name = "Card6";
+            this.Card6.Size = new System.Drawing.Size(30, 29);
+            this.Card6.TabIndex = 5;
+            this.Card6.Text = "X";
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.FloralWhite;
+            this.panel7.Controls.Add(this.Card5);
+            this.panel7.Location = new System.Drawing.Point(803, 191);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(155, 151);
+            this.panel7.TabIndex = 2;
+            // 
+            // Card5
+            // 
+            this.Card5.AutoSize = true;
+            this.Card5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Card5.Location = new System.Drawing.Point(62, 64);
+            this.Card5.Name = "Card5";
+            this.Card5.Size = new System.Drawing.Size(30, 29);
+            this.Card5.TabIndex = 4;
+            this.Card5.Text = "X";
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.FloralWhite;
+            this.panel6.Controls.Add(this.Card4);
+            this.panel6.Location = new System.Drawing.Point(598, 191);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(155, 151);
+            this.panel6.TabIndex = 2;
+            // 
+            // Card4
+            // 
+            this.Card4.AutoSize = true;
+            this.Card4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Card4.Location = new System.Drawing.Point(59, 64);
+            this.Card4.Name = "Card4";
+            this.Card4.Size = new System.Drawing.Size(30, 29);
+            this.Card4.TabIndex = 3;
+            this.Card4.Text = "X";
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.FloralWhite;
+            this.panel5.Controls.Add(this.Card3);
+            this.panel5.Location = new System.Drawing.Point(409, 191);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(155, 151);
+            this.panel5.TabIndex = 2;
+            // 
+            // Card3
+            // 
+            this.Card3.AutoSize = true;
+            this.Card3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Card3.Location = new System.Drawing.Point(64, 64);
+            this.Card3.Name = "Card3";
+            this.Card3.Size = new System.Drawing.Size(30, 29);
+            this.Card3.TabIndex = 2;
+            this.Card3.Text = "X";
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FloralWhite;
+            this.panel4.Controls.Add(this.Card2);
+            this.panel4.Location = new System.Drawing.Point(215, 191);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(155, 151);
+            this.panel4.TabIndex = 2;
+            // 
+            // Card2
+            // 
+            this.Card2.AutoSize = true;
+            this.Card2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Card2.Location = new System.Drawing.Point(61, 64);
+            this.Card2.Name = "Card2";
+            this.Card2.Size = new System.Drawing.Size(30, 29);
+            this.Card2.TabIndex = 1;
+            this.Card2.Text = "X";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FloralWhite;
+            this.panel3.Controls.Add(this.Card1);
+            this.panel3.Location = new System.Drawing.Point(24, 191);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(155, 151);
+            this.panel3.TabIndex = 1;
+            // 
+            // Card1
+            // 
+            this.Card1.AutoSize = true;
+            this.Card1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Card1.Location = new System.Drawing.Point(62, 64);
+            this.Card1.Name = "Card1";
+            this.Card1.Size = new System.Drawing.Size(30, 29);
+            this.Card1.TabIndex = 0;
+            this.Card1.Text = "X";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(612, 91);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cards On Table";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1234, 555);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel9.ResumeLayout(false);
+            this.panel9.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel8.ResumeLayout(false);
+            this.panel8.PerformLayout();
+            this.panel7.ResumeLayout(false);
+            this.panel7.PerformLayout();
+            this.panel6.ResumeLayout(false);
+            this.panel6.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel7;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Label Card1;
+        private System.Windows.Forms.Label Card6;
+        private System.Windows.Forms.Label Card5;
+        private System.Windows.Forms.Label Card4;
+        private System.Windows.Forms.Label Card3;
+        private System.Windows.Forms.Label Card2;
+        private System.Windows.Forms.Label TotalText;
+        private System.Windows.Forms.Button DrawButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Blackjack
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int total;
+        int total2;
+        int total3;
+        int total4;
+        int total5;
+        int total6;
+        int click;
+        int card1num;
+        int card2num;
+        int card3num;
+        int card4num;
+        int card5num;
+        int card6num;
+
+        Random r = new Random();
+
+        private void DrawButton_Click(object sender, EventArgs e)
+        {
+            click++;
+            if (TotalText.Text == "X")
+            {
+                TotalText.Text = "0";
+                int total = 0;
+            }
+            if (DrawButton.Text == "Begin")
+            {
+                DrawButton.Text = "Draw card";
+            }
+            if (click == 2)
+            {
+                card1num = r.Next(1, 10); //for ints
+                Card1.Text = card1num.ToString();
+                total = card1num;
+                TotalText.Text = total.ToString();
+            }
+            if (click == 3)
+            {
+                card2num = r.Next(1, 10);
+                Card2.Text = card2num.ToString();
+                total2 = card1num + card2num;
+                TotalText.Text = total2.ToString();             
+            }
+            if (click == 4)
+            {
+                card3num = r.Next(1, 10);
+                Card3.Text = card3num.ToString();
+                total3 = card1num + card2num + card3num;
+                TotalText.Text = total3.ToString();               
+            }
+            if (click == 5)
+            {
+                card4num = r.Next(1, 10);
+                Card4.Text = card4num.ToString();
+                total4 = card1num + card2num + card3num + card4num;
+                TotalText.Text = total4.ToString();
+            }
+            if (click == 6)
+            {
+                card5num = r.Next(1, 10);
+                Card5.Text = card5num.ToString();
+                total5 = card1num + card2num + card3num + card4num + card5num;
+                TotalText.Text = total5.ToString();
+            }
+            if (click == 7)
+            {
+                card6num = r.Next(1, 10);
+                Card6.Text = card6num.ToString();
+                total6 = card1num + card2num + card3num + card4num + card5num + card6num;
+                TotalText.Text = total6.ToString();
+            }
+        }
+    }
+}
